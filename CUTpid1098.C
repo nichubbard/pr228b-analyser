6fea7dc8f0524c001139ed9fd2f6901ae0d1813e
Nic Hubbard <nicolas.hubbard@york.ac.uk>
2015-09-06 15:28:42 +0200

    A new CUT file

---
+++ CUTpid1098.C
@@ -0,0 +1,19 @@
+{
+//========= Macro generated from object: CUTpid/Graph
+//========= by ROOT version6.04/02
+   
+   cutg = new TCutG("CUTpid",8);
+   cutg->SetVarX("tof");
+   cutg->SetVarY("pad1");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,3494.76,1801.17);
+   cutg->SetPoint(1,3462.25,1676.81);
+   cutg->SetPoint(2,3454.22,1105.57);
+   cutg->SetPoint(3,3502.79,833.549);
+   cutg->SetPoint(4,3542.06,1191.06);
+   cutg->SetPoint(5,3534.88,1735.1);
+   cutg->SetPoint(6,3503.21,1832.25);
+   cutg->SetPoint(7,3494.76,1801.17);
+   cutg->Draw("");
+}
