63d71a5ae62fa92c6900b06a5c07c0b9c440ffc3
Nic Hubbard <nicolas.hubbard@york.ac.uk>
2015-10-15 14:17:08 +0200

    Add X1vsTOF CUT (not that useful though)

---
+++ analysis/CUTX1tof1098.C
@@ -0,0 +1,18 @@
+{
+//========= Macro generated from object: CUTX1tof/Graph
+//========= by ROOT version6.04/02
+   
+   TCutG *cutg = new TCutG("CUTX1tof",7);
+   cutg->SetVarX("X1pos");
+   cutg->SetVarY("tof");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,170.594,3562.09);
+   cutg->SetPoint(1,691.971,3551.26);
+   cutg->SetPoint(2,761.314,3532.45);
+   cutg->SetPoint(3,758.707,3455.51);
+   cutg->SetPoint(4,184.672,3452.66);
+   cutg->SetPoint(5,165.902,3495.97);
+   cutg->SetPoint(6,170.594,3562.09);
+   cutg->Draw("");
+}
