b04f6faede87b4f9ad53a9739b1f6e62f38981b3
Nic Hubbard <nicolas.hubbard@york.ac.uk>
2015-10-21 16:56:53 +0200

    Add cut from runs after 1109

---
+++ analysis/CUTpid1109.C
@@ -0,0 +1,20 @@
+{
+//========= Macro generated from object: CUTpid/Graph
+//========= by ROOT version6.04/02
+   
+   TCutG *cutg = new TCutG("CUTpid",9);
+   cutg->SetVarX("tof");
+   cutg->SetVarY("pad1");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,3304.6,1844.79);
+   cutg->SetPoint(1,3327.82,1815.41);
+   cutg->SetPoint(2,3328,1395.72);
+   cutg->SetPoint(3,3312.05,955.049);
+   cutg->SetPoint(4,3252.31,1110.33);
+   cutg->SetPoint(5,3254.26,1561.5);
+   cutg->SetPoint(6,3276.59,1645.44);
+   cutg->SetPoint(7,3277.66,1809.12);
+   cutg->SetPoint(8,3304.6,1844.79);
+   cutg->Draw("");
+}
