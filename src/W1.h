c54b640986676c6ea3cf9a439dbba2a4d51982a3
Nic Hubbard <nicolas.hubbard@york.ac.uk>
2015-11-20 14:57:15 +0100

    Cleanup W1 code

--- src/W1.h
+++ src/W1.h
@@ -1,32 +1,28 @@
 #ifndef W1_h
 #define W1_h 1
 
-/* Header file for the code sorting W1 data into something reasonable and sensible. 
+/* Header file for the code sorting W1 data into something reasonable and sensible.
  * Author: Phil Adsley
  * August 2014
  * padsley@gmail.com for queries/comments
  */
 
-#include <TCutG.h>
-#include <vector>
 #include <TFile.h>
+#include <TROOT.h>
 #include <TTree.h>
 #include <vector>
-#include <TROOT.h>
+
 #include "multiTDC.h"
+#include "SiliconData.h"
 
 SiliconData *W1SiliconSort(float *ADC_import, int ntdc, int *TDC_channel_import, float *TDC_value_import);
 
-bool W1SuppressChannel(int Channel);
 double W1EnergyCalc(int Channel, double ADCValue);
 double W1ThetaCalc(int FrontChannel, int BackChannel);
 double W1PhiCalc(int FrontChannel, int BackChannel);
 bool W1FrontBackTest(int FrontChannel, int BackChannel, double FrontEnergy, double BackEnergy, SiliconData *si);
 bool W1ADCTDCChannelTestPSide(int ADCChannel, int TDCChannel);
 bool W1ADCTDCChannelTestNSide(int ADCChannel, int TDCChannel);
-
-void W1Init();
-void W1LoadCuts(SiliconData *si);
 
 int W1DetHitNumber(int FrontChannel, int BackChannel);
 
@@ -40,3 +36,4 @@ bool W1TDCFrontBackTest(int TDCFrontChannel, int TDCBackChannel);
 int W1TDCIdentifyDetector(int TDCFrontChannel, int TDCBackChannel);
 
 #endif
+
