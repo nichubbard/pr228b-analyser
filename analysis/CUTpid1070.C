d2578631bef2729c892ea9278043157f476323ed
Nic Hubbard <nicolas.hubbard@york.ac.uk>
2015-12-16 11:27:00 +0100

    Add PID cuts for new periods

---
+++ analysis/CUTpid1070.C
@@ -0,0 +1,21 @@
+{
+//========= Macro generated from object: CUTpid/Graph
+//========= by ROOT version6.04/08
+   
+   cutg = new TCutG("CUTpid",10);
+   cutg->SetVarX("tof");
+   cutg->SetVarY("pad1");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,3471.53,1571.03);
+   cutg->SetPoint(1,3471.53,1145.73);
+   cutg->SetPoint(2,3520.96,944.77);
+   cutg->SetPoint(3,3520.53,1113.02);
+   cutg->SetPoint(4,3528.69,1113.02);
+   cutg->SetPoint(5,3530.41,1313.98);
+   cutg->SetPoint(6,3554.91,1412.13);
+   cutg->SetPoint(7,3553.19,1566.35);
+   cutg->SetPoint(8,3521.39,1650.48);
+   cutg->SetPoint(9,3471.53,1571.03);
+   cutg->Draw("");
+}
---
+++ analysis/CUTpid1107.C
@@ -0,0 +1,20 @@
+{
+//========= Macro generated from object: CUTpid/Graph
+//========= by ROOT version6.04/08
+   
+   TCutG *cutg = new TCutG("CUTpid",9);
+   cutg->SetVarX("tof");
+   cutg->SetVarY("pad1");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,3249.16,1575.64);
+   cutg->SetPoint(1,3257.07,1089.97);
+   cutg->SetPoint(2,3304.52,914.809);
+   cutg->SetPoint(3,3316.38,978.503);
+   cutg->SetPoint(4,3320.33,1320.86);
+   cutg->SetPoint(5,3342.74,1472.13);
+   cutg->SetPoint(6,3340.1,1575.64);
+   cutg->SetPoint(7,3291.34,1647.29);
+   cutg->SetPoint(8,3249.16,1575.64);
+   cutg->Draw("");
+}
